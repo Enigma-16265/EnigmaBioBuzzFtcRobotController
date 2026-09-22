9a2237abcc0d0a8236e1e0e63bbe33a3fda45be3
BudsterGaming <budstergaming@binaryflood.com>
2026-09-22 17:14:26 -0600

    most dubious color detection that seems to work. 1 color sensor seems to work fine even with a perfectly lined up hole through nectar/pollen.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DualColorSensorTest.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+
+enum BallColors {
+    RED,
+    BLUE,
+    YELLOW,
+    UNKNOWN
+}
+
+@TeleOp
+public class DualColorSensorTest extends OpMode {
+    private RevColorSensorV3 colorSensor1;
+
+    NormalizedRGBA colors;
+
+    float r, g, b;
+
+
+
+
+    private BallColors colorDetection() {
+        if (r > g * 1.4 && r > b * 1.4) {
+            return BallColors.RED;
+        }
+        else if (b > r * 1.4 && b > g * 1.2) {
+            return BallColors.BLUE;
+        }
+        else if (r > b * 1.3 && g > b * 1.3) {
+            return BallColors.YELLOW;
+        }
+        else {
+            return BallColors.UNKNOWN;
+        }
+    }
+
+    private void telemetry() {
+        telemetry.addData("guess", colorDetection());
+
+        telemetry.update();
+    }
+
+    private void updateRGB() {
+        colors = colorSensor1.getNormalizedColors();
+        r = colors.red;
+        g = colors.green;
+        b = colors.blue;
+    }
+
+    @Override
+    public void init() {
+        colorSensor1 = hardwareMap.get(RevColorSensorV3.class, "colorSensor1");
+    }
+
+    @Override
+    public void loop() {
+        updateRGB();
+        colorDetection();
+        telemetry();
+    }
+}
